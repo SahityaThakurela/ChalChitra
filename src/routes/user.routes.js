8fe763ff25ec95aa11abf7befe7533c0ef675235
SahityaThakurela <s1529163@gmail.com>
2026-05-31 10:12:34 +0530

    Adding the routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import { loginUser, registerUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import {    loginUser, 
+            registerUser, 
+            logoutUser, 
+            refreshAccessToken, 
+            changePassword, 
+            getCurrentUser, 
+            updateAccountDetails, 
+            updateUserAvatar, 
+            updateUserCover, 
+            getUserChannelProfile, 
+            getUserHistory 
+    } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -24,8 +35,15 @@ router.route("/login").post(loginUser)
 
 // secured Routes -> need to verify the auth by middleware like verifyJWT here
 router.route("/logout").post(verifyJWT, logoutUser)
-
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("change-password").post(verifyJWT, changePassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+// patch for selected updates post update all the fields
+router.route("/update-account-details").patch(verifyJWT, updateAccountDetails) 
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar) //doubt
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCover)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/user-history").get(verifyJWT, getUserHistory)
 
 export default router
 
