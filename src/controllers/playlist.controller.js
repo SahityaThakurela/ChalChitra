d8e6f0a0602e8942d77a743f5b1edc5c5566ddbb
SahityaThakurela <s1529163@gmail.com>
2026-06-06 15:59:54 +0530

    Toggle subscription controller

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,7 +3,6 @@ import { Playlist } from "../models/playlist.model.js"
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
-import { useId } from "react"
 
 
 // // $push - Add element to array
--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -7,8 +7,66 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
+    const {channelId} = req.params  //channel id is basically a user id
     // TODO: toggle subscription
+    if(!channelId){
+        throw new ApiError(400, "please enter channel id")
+    }
+
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400, "enter valid channel id")
+    }
+
+    if(channelId.toString() === req.user?._id.toString()){
+        throw new ApiError(400, "You can't like your own channel")
+    }
+    
+    const subscribed = await Subscription.findOne({ 
+        channel: channelId,
+        subscriber: req.user?._id
+    })
+    
+
+    if(!subscribed){
+        const isSubscribed = await Subscription.create({
+            subscriber: req.user?._id,
+            channel: channelId
+        })
+
+        if (!isSubscribed) {
+        throw new ApiError(500, "Something went wrong while subscribing");
+    }
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { isSubscribed: true },
+                "Channel get subscribed successfully"
+            )
+        )
+    }else{
+        const isSubscribed = await Subscription.findOneAndDelete({ 
+            subscriber: req.user?._id,
+            channel: channelId
+        })
+
+        if (!isSubscribed) {
+        throw new ApiError(500, "Something went wrong while unSubscribing");
+    }
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { isSubscibed: false },
+                "Channel get unSubscribed successfully"
+            )
+        )
+    }
+
 })
 
 // controller to return subscriber list of a channel
